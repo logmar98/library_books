d857b033a09802a33b2551e69d5bb2c3b68aa7df
logmar98 <marwanarbib15@gmail.com>
2024-03-22 10:22:15 +0000

    i just finish section library that will contain the shelfs

---
+++ client/src/components/LibraryRow/LibraryRows.jsx
@@ -0,0 +1,13 @@
+import styles from './LibraryRows.module.css';
+
+function LibraryRows() {
+    
+
+    return (
+        <div className={styles.librarySection}>
+            <h1>libare</h1>
+        </div>
+    );
+}
+
+export default LibraryRows;
--- client/src/components/LibrarySection/LibrarySection.jsx
+++ client/src/components/LibrarySection/LibrarySection.jsx
@@ -1,15 +1,31 @@
 import styles from './LibrarySection.module.css';
+import { useRef } from 'react';
+import LibraryRows from '../LibraryRow/LibraryRows.jsx';
 
 
 function LibrarySection() {
-    
-    return (
-        <div className={styles.container}>
-            <div className={styles.headerContainer}>
-                <p>some</p>
-            </div>
+  const detailsRef = useRef();
+  const summaryRef = useRef();
+
+  function resizebyhight() {
+    if (detailsRef.current.open) {
+      summaryRef.current.classList.add(styles.headerInactive);
+    } else {
+      summaryRef.current.classList.remove(styles.headerInactive);
+    }
+  }
+
+  return (
+    <div>
+      <details onToggle={resizebyhight} ref={detailsRef} id='container' className={styles.container}>
+        <summary ref={summaryRef} id='header' className={styles.containerHeader}>Read Later</summary>
+        <div className={styles.containerBody}>
+            <LibraryRows />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque minus laudantium molestias atque repudiandae quo deleniti libero eius dolore necessitatibus, consequuntur ducimus reiciendis obcaecati laboriosam, voluptatibus dicta quia! Laboriosam, consequatur!</p>
         </div>
-    );
+      </details>
+    </div>
+  );
 }
 
 export default LibrarySection;
